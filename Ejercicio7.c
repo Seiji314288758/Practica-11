2d32755b8ea40abdd9ab2f955b035f0ddc9fae5e
Seiji314288758 <nietoseiji@gmail.com>
2025-11-13 14:24:13 -0600

    Create Ejercicio7.c

---
+++ Ejercicio7.c
@@ -0,0 +1,45 @@
+/*Victor Seiji Nieto Morita
+practica 11 ejercicio 7*/
+
+#include <stdio.h>
+//Declaracion de las funciones
+int suma(int,int);
+static int resta(int,int);
+int producto(int,int);
+static int cociente (int,int);
+
+int suma(int,int);
+//static int resta(int,int);
+int producto(int,int);
+//static int cociente (int,int);
+int main()
+{
+printf("5 + 7 = %i\n",suma(5,7));
+//printf("9 - 77 = %d\n",resta(9,77));
+printf("6 * 8 = %i\n",producto(6,8));
+//printf("7 / 2 = %d\n",cociente(7,2));
+}
+
+
+
+
+
+
+
+//Implementacion de las funciones
+int suma (int a, int b)
+{
+  return a + b;
+}
+  static int resta (int a, int b)
+{
+  return a - b;
+}
+int producto (int a, int b)
+{
+  return (int)(a*b);
+}
+  static int cociente (int a, int b)
+{
+  return (int)(a/b);
+}

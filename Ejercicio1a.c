35601214a31a2ff924e1121bc98aec97c87dd6e7
Seiji314288758 <nietoseiji@gmail.com>
2025-11-13 13:32:56 -0600

    Create Ejercicio1a.c

---
+++ Ejercicio1a.c
@@ -0,0 +1,29 @@
+/*Victor Seiji Nieto Morita
+practica 11 ejercicio 1a*/
+
+#include <stdio.h>
+#include <string.h>
+
+// Prototipo o firma de las funciones del programa
+void imprimir(char[]);
+void imprimir2(char[]);
+
+// Definición o implementación de la función main
+int main()
+{
+  char nombre[] = "Facultad de Ingenieria";
+  imprimir(nombre);
+  return 0;
+}
+
+// Implementación de las funciones del programa
+void imprimir2(char s[])
+{
+ int tam;
+ tam =strlen()
+ for ( tam=strlen(s)-1 ; tam>=0 ; tam--)
+ printf("%c", s[tam]);
+ printf("\n");
+}
+
+
